c3530deaf170182c7dd58eee2d08a300e3a59d94
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <dounia.elmoutaoukil@gmail.com>
2025-09-02 16:10:40 +0100

    fix(utils): manhattan distance with abs

--- utils/distances.py
+++ utils/distances.py
@@ -1,9 +1,4 @@
 def manhattan(a, b):
-    a = (1, 1)
-    b = (4, 5)
-
-    d_x = b[0] - a[0]
-    d_y = b[1] - a[1]
-
-    distance = abs(d_x + d_y)
-    return distance 
+    d_x = abs(b[0] - a[0])
+    d_y = abs(b[1] - a[1])
+    return d_x + d_y
